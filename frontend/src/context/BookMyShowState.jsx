21e6ee76ab54c97e4a1d9ea78a418279be3c9f00
arnavsawant9 <arnav.sawantbhosale@gmail.com>
2025-01-26 09:50:20 +0530

    made some more changes in frontend

--- frontend/src/context/BookMyShowState.jsx
+++ frontend/src/context/BookMyShowState.jsx
@@ -14,6 +14,55 @@ const BookMyShowState = (props) => {
       D2:""
     })
     const [lastBookingDetails, setLastBookingDetails] = useState(null)
+
+    const handlePostBooking = async() =>{  //here you are writing logic for post method
+      const response = await fetch(`http://localhost:8080`, {
+        method: "Post",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body:JSON.stringify({movie: movie, slot: time, seats: seatNumber}),
+      })
+
+      const data = await response.json();
+
+      setErrorPopup(true)
+      setErrorMessage(data.message)
+
+      if(response.status === 200){
+        setTime("")
+        setMovie("")
+
+        setLastBookingDetails(data.data)
+        window.localStorage.clear()
+      }
+    }
+
+    const handleGetBooking = async() =>{
+      const response = await fetch(`http://localhost:8080/api/booking`, {
+        method: "Get"
+      })
+
+      const data = await response.json()
+      setLastBookingDetails(data.data);
+    }
+
+    useEffect(()=>{
+      const movie = window.localStorage.getItem("movie")
+      const slot = window.localStorage.getItem("slot")
+      const seats = JSON.parse(window.localStorage.getItem("seats"))
+
+      if(movie){
+        setMovie(movie)
+      }
+      if(slot){
+        setTime(slot)
+      }
+      if(seat){
+        setSeatNumber(seats)
+      }
+    },[])
+
   return (
     <BookMyShowContext.Provider value={{movie,setMovie,seat,setSeat,time, setTime,seatNumber, setSeatNumber,lastBookingDetails, setLastBookingDetails}}>
         {props.children}
